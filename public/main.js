b5178fcaa12b3f2be35796bf9a5849ec04ef4db0
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2021-01-07 23:20:44 +0530

    updated

---
+++ public/main.js
@@ -0,0 +1,32 @@
+
+
+const getLinks = async () => {
+    let uri = 'https://links-list-app.herokuapp.com/links?_sort=name&_order=asc';
+    const res = await fetch(uri);
+    const links = await res.json();
+
+    // console.log(links);
+    
+    
+    links.forEach(i => {
+        var link = document.getElementById('links');
+        let template = `
+            <div class="fixed-content card " data-aos="fade-up">
+                <h2>${i.name}</h2>
+                <a href="${i.link}">Website</a>
+                <img src="${i.image}" class="img" alt="portfolio image">
+                <a href="./detail.html?id=${i.id}">Read more</a>
+            </div>
+        `;
+
+        // link.innerHTML = template;
+        link.insertAdjacentHTML('beforeend', template);
+    });
+
+
+
+}
+
+window.addEventListener('DOMContentLoaded', () => getLinks());
+
+// getLinks();
