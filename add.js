1d21231c9c117837b0a7478c19ab4044411ae743
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-23 04:28:07 +0530

    Update add.js

--- add.js
+++ add.js
@@ -11,7 +11,7 @@ const createPost = async (e) => {
   }
 
   
-  await fetch(`https://linklist-vert.vercel.app/links/`, {
+  await fetch(`https://links-list-app.herokuapp.com/links/`, {
     method: 'POST',
     body: JSON.stringify(doc),
     headers: { 'Content-Type': 'application/json' }
