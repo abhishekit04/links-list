8a8e0c8870e53e518b9ed496290ba232e6004766
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-23 14:36:26 +0530

    Update add.js

--- public/add.js
+++ public/add.js
@@ -17,7 +17,7 @@ const createPost = async (e) => {
   // })
 
   
-  await fetch(`https://links-list-app.herokuapp.com/links/`, {
+  await fetch(`https://singlebuck.herokuapp.com/links/`, {
     method: 'POST',
     body: JSON.stringify(doc),
     headers: { 'Content-Type': 'application/json' }
